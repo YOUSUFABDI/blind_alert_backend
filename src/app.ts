cd2ca6df73b6f8371227e2672222aa8e6f9503d6
YOUSUF <yufis0844@gmail.com>
2024-07-03 10:57:06 +0300

    w-on

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express"
+import prisma from "../prisma/client"
 
 const app = express()
 
 app.use(express.json())
 
 app.get("/api", async (req, res) => {
-  res.json({ message: "Hello!" })
+  const test = await prisma.test.findMany()
+  res.json({ message: test })
 })
 
 export default app
